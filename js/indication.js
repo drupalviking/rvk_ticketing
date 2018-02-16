683d04cf3823d5e401d66d44332fcc6b4479e843
Franz Eiriksson <franz@kosmosogkaos.is>
2018-02-16 12:50:52 +0000

    changed map animation

--- js/indication.js
+++ js/indication.js
@@ -4,7 +4,13 @@
   Drupal.behaviors.accordion = {
     attach: function (context, settings) {
      
-      $( '.toggle-icon' ).click(function() {
+      $( '#edit-map-fieldset-body .toggle-icon' ).click(function() {
+        $(this).parent().next('.collapse').toggleClass('show');
+        $(this).toggleClass('flip');
+        $(this).parent().parent().parent().parent().parent().parent().toggleClass('move');
+      });
+
+      $( '#edit-image-fieldset-body .toggle-icon' ).click(function() {
         $(this).parent().next('.collapse').toggleClass('show');
         $(this).toggleClass('flip');
       });
